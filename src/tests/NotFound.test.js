8713026d10e70c032ecd04e221ad0dacbe8e563d
Renato Mendes <mendeslrenato@gmail.com>
2022-07-15 19:48:56 -0300

    Requirement 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from './handleWithRouter';
+
+describe('Requisito 4', () => {
+  test(`Teste se a página contém um heading h2
+  com o texto Page requested not found 😭`, () => {
+    renderWithRouter(<NotFound />);
+    const notFoundTitle = screen.getByRole('heading', {
+      name: /page requested not found/i,
+    });
+    expect(notFoundTitle).toBeDefined();
+  });
+  test(`Teste se a página mostra a imagem
+  https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif`, () => {
+    renderWithRouter(<NotFound />);
+    const imgNotFound = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    expect(imgNotFound).toBeDefined();
+    expect(imgNotFound).toHaveProperty('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
